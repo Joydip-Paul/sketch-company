7f49706f54fada551722327cfa9c53ba20db992a
Joydip Paul <joydippaul72@gmail.com>
2020-12-31 13:24:29 +0600

    90% done

---
+++ js/app.js
@@ -0,0 +1,23 @@
+function toggle(){
+    var header = document.getElementById("header");
+    header.classList.toggle('active');
+}
+
+
+// function clicko(){
+//     document.getElementById("mydropdown").classList
+//     .toggle("show");
+// }
+// window.onclick = function(event){
+//     if(!event.target.matches('.dropbtn')){
+//         var dropdowns = document.getElementsByClassName
+//         ("dropdown-content");
+//         var i;
+//         for(i=0;i<dropdowns.length;i++){
+//             var opendropdown = dropdowns[i];
+//             if(opendropdown.classList.contains('show')){
+//                 // opendropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
